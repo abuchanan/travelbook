c5a27c4ae48c3646c62d4e24eb1155a4604fca55
Alex Buchanan <buchanae@gmail.com>
2015-11-22 23:00:29 +1100

    switch to Track and keyframes

--- app/stores/timeline.js
+++ app/stores/timeline.js
@@ -9,16 +9,74 @@ import IntervalTree from 'interval-tree';
 import { LocationActions, MapActions, TimelineActions } from '../actions';
 
 
+class Track {
+  constructor() {
+    this.keyframes = [];
+    this.noValue = {};
+    this.lastValue = this.noValue;
+  }
+
+  addKeyframe(time, value) {
+    for (var i = 0; i < this.keyframes.length; i++) {
+      var keyframe = this.keyframes[i];
+
+      if (keyframe[0] == time) {
+        keyframes[1] = value;
+      } else if (time < keyframe[0]) {
+        this.keyframes.splice(i, 0, [time, value]);
+        return;
+      }
+    }
+
+    this.keyframes.push([time, value]);
+  }
+
+  update(time) {
+    var i = 0;
+    while (time >= this.keyframes[i][0]) {
+      i++;
+    }
+
+    if (i > 0) {
+      i -= 1;
+    }
+    var value = this.keyframes[i][1];
+    if (typeof value == "function") {
+      value = value(time);
+    }
+    if (value !== this.lastValue) {
+      this.onChange(value);
+      this.lastValue = value;
+    }
+  }
+
+  onChange(value) {
+  }
+}
+
+
+class LocationTrack extends Track {
+  onChange(value) {
+    console.log(value);
+    MapActions.setCenter(value);
+  }
+}
+
+
+
 const TimelineStore = Reflux.createStore({
 
   listenables: TimelineActions,
-  events: null,
+  tracks: [],
 
   init() {
     this.fetchGeoData();
   },
 
   fetchGeoData() {
+    var locationTrack = new LocationTrack();
+    this.tracks.push(locationTrack);
+
     jQuery.ajax({
         url: "/travels.geojson",
         dataType: "json",
@@ -26,41 +84,20 @@ const TimelineStore = Reflux.createStore({
         context: this,
         success: function(data) {
           var t = 0;
-          var events = [];
 
           data.features.forEach(feature => {
-            events.push({
-              name: 'setCenter',
-              coordinates: feature.geometry.coordinates,
-              startTime: t,
-              endTime: t + 1,
-            });
-
+            locationTrack.addKeyframe(t, feature.geometry.coordinates);
             t += 1500;
           });
 
-          var tree = new IntervalTree(Math.floor(t / 2));
-          events.forEach(event => {
-            tree.add([event.startTime, event.endTime, event]);
-          });
-          this.events = tree;
           this.endTime = t;
           console.log('loaded');
         }
     });
   },
 
-  onSetCenter(event, dt) {
-    MapActions.setCenter(event.coordinates);
-  },
-
   onFrame(time, previousTime) {
-    console.log(time, previousTime);
-
-    this.events.search(previousTime, time).forEach(res => {
-      var event = res.data[2];
-      TimelineActions[event.name](event, time);
-    });
+    this.tracks.forEach(track => track.update(time));
 
     if (time >= this.endTime) {
       this.stop();
